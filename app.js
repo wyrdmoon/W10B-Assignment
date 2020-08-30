19ca4f6f610e09fe8c68b4947ed8dbe0b3e3136b
wyrdmoon <criscorcoran@gmail.com>
2020-08-29 23:05:27 -0600

    Assignment w10b

---
+++ app.js
@@ -0,0 +1,12 @@
+function chooseMetal() {
+    Cookies.set("metal","silver");
+    window.open(selection.hmtl);
+}
+function chooseMetal() {
+    Cookies.set("metal","gold");
+    window.open(selection.hmtl);
+}
+function chooseMetal() {
+    Cookies.set("metal","platinum");
+    window.open(selection.hmtl);
+}
---
+++ selection.js
@@ -0,0 +1,3 @@
+var selectedMetal =Cookies.get("metal");
+var selectedMetal = document.getElementById(chosen-metal);
+metal.innerHTML = selectedMetal;
